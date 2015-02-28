9fd4510e6fff80a792007d34bbcb6ef6fbd5457e
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2015-02-28 22:03:24 +0100

    tianchi_dsds: Add support for otapackage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,8 @@ include device/sony/yukon/BoardConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := D5322
 
+TARGET_RECOVERY_FSTAB = device/sony/tianchi_dsds/rootdir/fstab.yukon
+
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2399141888
 #Reserve space for data encryption (5112839168-16384)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5112822784
