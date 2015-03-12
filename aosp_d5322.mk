fe1d29d5b1460a822331d6be43c3754b8dac1778
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2015-03-12 15:14:23 +0100

    tianchi dsds support

--- aosp_d5322.mk
+++ aosp_d5322.mk
@@ -22,31 +22,31 @@ BOARD_HAS_DIM_BACKLIGHT := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/yukon/device.mk)
-$(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
+$(call inherit-product, vendor/sony/tianchi_dsds/tianchi_dsds-vendor.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/tianchi/overlay
+    device/sony/tianchi_dsds/overlay
 
 PRODUCT_COPY_FILES += \
-    device/sony/tianchi/rootdir/system/usr/idc/cyttsp4_mt.idc:system/usr/idc/cyttsp4_mt.idc \
-    device/sony/tianchi/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    device/sony/tianchi/rootdir/system/etc/sensor_def_somc.conf:system/etc/sensor_def_somc.conf \
-    device/sony/tianchi/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
-    device/sony/tianchi/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/sony/tianchi/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
-    device/sony/tianchi/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/tianchi/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    device/sony/tianchi/rootdir/init.yukon.dev.rc:root/init.yukon.dev.rc \
-    device/sony/tianchi/rootdir/fstab.yukon:root/fstab.yukon
+    device/sony/tianchi_dsds/rootdir/system/usr/idc/cyttsp4_mt.idc:system/usr/idc/cyttsp4_mt.idc \
+    device/sony/tianchi_dsds/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
+    device/sony/tianchi_dsds/rootdir/system/etc/sensor_def_somc.conf:system/etc/sensor_def_somc.conf \
+    device/sony/tianchi_dsds/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
+    device/sony/tianchi_dsds/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/tianchi_dsds/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/tianchi_dsds/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/sony/tianchi_dsds/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/tianchi_dsds/rootdir/init.yukon.dev.rc:root/init.yukon.dev.rc \
+    device/sony/tianchi_dsds/rootdir/fstab.yukon:root/fstab.yukon
 
 PRODUCT_PACKAGES += \
     keystore.msm8226
 
-PRODUCT_NAME := aosp_d5303
-PRODUCT_DEVICE := tianchi
+PRODUCT_NAME := aosp_d5322
+PRODUCT_DEVICE := tianchi_dsds
 PRODUCT_BRAND := Sony
-PRODUCT_MODEL := Xperia T2 Ultra (AOSP)
+PRODUCT_MODEL := Xperia T2 Ultra Dual (AOSP)
 PRODUCT_LOCALES += xhdpi hdpi
 PRODUCT_MANUFACTURER := Sony
 
